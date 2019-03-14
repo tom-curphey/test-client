5de667d1b2aca656e07bf218deb0e6101b4f71c9
muppet-training <tomcurphey12@gmail.com>
2019-03-14 22:33:47 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    fetch('/api/data')
+      .then(res => res.json())
+      .then(res => console.log(res));
+  }
+
   render() {
     return (
       <div className="App">
